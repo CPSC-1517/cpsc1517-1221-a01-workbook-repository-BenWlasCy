62ac81e41430408e113fcf665406b997722be410
BenWlasCy <bwlasichuk1@nait.ca>
2023-01-11 14:48:46 -0700

    Jan 11

---
+++ NhlSolution/NhlSystemClassLibrary/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystemClassLibrary
+{
+    internal class Team
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Arena { get; set; }
+        public Conference Conference { get; set; }
+        public Division Division { get; set; }
+    }
+}
